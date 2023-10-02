6a6fd0157a41c6e77e3877be5df6d94cda92c253
renataguis <renata.souza@sga.pucminas.br>
2023-10-02 14:40:37 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// seleção de elementos
+const todoform = document.querySelector("#todo-form");
+const todoimput = document.querySelector("#todo-imput");
+const todolist = document.querySelector("#todo-list");
+const editimput = document.querySelector("#todo-form");
+const canceleditbtn = document.querySelector("#todo-cancel-edit-btn");
+
+// funções
+todoform.addEventListener( "submit",(e) =>) {
+    
+}
+
+// eventos
